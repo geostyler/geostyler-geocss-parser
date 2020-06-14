59b1802a376960d09ed5bdfbd869bb6479c09fbe
allyoucanmap <bovio.stefano@gmail.com>
2020-06-15 00:50:12 +0200

    refactor of read style

--- src/GeoCSSStyleParser.spec.ts
+++ src/GeoCSSStyleParser.spec.ts
@@ -2,6 +2,13 @@ import * as fs from 'fs';
 import GeoCSSStyleParser from './GeoCSSStyleParser';
 import { Style } from 'geostyler-style';
 
+import line_graphicStroke from '../data/styles/line_graphicStroke';
+import line_graphicStroke_externalGraphic from '../data/styles/line_graphicStroke_externalGraphic';
+import line_perpendicularOffset from '../data/styles/line_perpendicularOffset';
+import line_simpleline from '../data/styles/line_simpleline';
+// import multi_simplelineLabel from '../data/styles/multi_simplelineLabel';
+import point_externalgraphic from '../data/styles/point_externalgraphic';
+import polygon_graphicFill from '../data/styles/polygon_graphicFill';
 import polygon_transparentpolygon from '../data/styles/polygon_transparentpolygon';
 
 it('GeoCSSStyleParser is defined', () => {
@@ -20,7 +27,79 @@ describe('GeoCSSStyleParser implements StyleParser', () => {
       expect(styleParser.readStyle).toBeDefined();
     });
 
-    it('can read a GeoCSS PolygonSymbolizer', () => {
+    it('can read a line with symbol', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/line_graphicStroke.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(line_graphicStroke);
+        });
+    });
+
+    it('can read a line with external graphic', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/line_graphicStroke_externalGraphic.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(line_graphicStroke_externalGraphic);
+        });
+    });
+
+    it('can read a line with perpendicular offset', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/line_perpendicularOffset.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(line_perpendicularOffset);
+        });
+    });
+
+    it('can read a simple line', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/line_simpleline.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(line_simpleline);
+        });
+    });
+    /*
+    // TODO: fix labels [name]' entity'
+    it('can read a line with label', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/multi_simplelineLabel.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          console.log(JSON.stringify(geoStylerStyle));
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(multi_simplelineLabel);
+        });
+    });
+    */
+    it('can read point with external graphic', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/point_externalgraphic.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(point_externalgraphic);
+        });
+    });
+
+    it('can read a polygon with graphic', () => {
+      expect.assertions(2);
+      const geocss = fs.readFileSync('./data/geocsss/polygon_graphicFill.css', 'utf8');
+      return styleParser.readStyle(geocss)
+        .then((geoStylerStyle: Style) => {
+          expect(geoStylerStyle).toBeDefined();
+          expect(geoStylerStyle).toEqual(polygon_graphicFill);
+        });
+    });
+
+    it('can read a transparent polygon', () => {
       expect.assertions(2);
       const geocss = fs.readFileSync('./data/geocsss/polygon_transparentpolygon.css', 'utf8');
       return styleParser.readStyle(geocss)
--- src/GeoCSSStyleParser.ts
+++ src/GeoCSSStyleParser.ts
@@ -7,31 +7,28 @@ import {
   // ComparisonOperator,
   // CombinationOperator,
   ScaleDenominator,
-  // PointSymbolizer,
+  PointSymbolizer,
   Symbolizer,
-  // IconSymbolizer,
+  IconSymbolizer,
   LineSymbolizer,
   FillSymbolizer,
-  // TextSymbolizer,
-  // RasterSymbolizer,
+  TextSymbolizer,
+  RasterSymbolizer,
   // ColorMap,
   // ChannelSelection,
   // ComparisonFilter,
-  // MarkSymbolizer,
-  // WellKnownName,
+  MarkSymbolizer,
+  WellKnownName,
   // ColorMapEntry,
   // Channel,
   // ContrastEnhancement,
   // StrMatchesFunctionFilter
 } from 'geostyler-style';
-import geoCSS2JSON from './utils/geoCSS2JSON';
-import GeoStylerCQLParser from 'geostyler-cql-parser';
 
-const _trim = require('lodash/trim');
-const _find = require('lodash/find');
-const _join = require('lodash/join');
-
-const geoStylerCQLParser = new GeoStylerCQLParser();
+const flatGeoCSS = require('flat-geo-css').default;
+const _castArray = require('lodash/castArray');
+const _isArray = require('lodash/join');
+const _isNil = require('lodash/isNil');
 
 export class GeoCSSStyleParser implements StyleParser {
   /**
@@ -46,82 +43,220 @@ export class GeoCSSStyleParser implements StyleParser {
   }
 
   getFilterFromSelectors(selectors: any): Filter | undefined {
-    const allSelector = _find(selectors, (selector: string) => selector === '*');
-    if (allSelector) {
-      return undefined;
-    }
-    const filters = selectors
-      .map((selector: string) =>
-        _trim(selector
-          .replace(/\*|\[|\]/g, '')
-          .replace(' and ', ' AND ')
-          .replace(' or ', ' OR '))
-      );
-    return geoStylerCQLParser.read(_join(filters, ' OR '));
-  }
-
-  getScaleDenominatorFromSelectors(): ScaleDenominator | undefined {
     return undefined;
   }
 
-  getLineSymbolizerFromGeoCSSRule(geoCSSRule: any): LineSymbolizer {
-    const { rule } = geoCSSRule;
-    const kind = 'Line';
-    const color = rule.stroke;
-    const width = parseFloat(rule['stroke-width']);
-    const opacity = parseFloat(rule['stroke-opacity']);
-    const join = rule['stroke-linejoin'];
-    const cap = rule['stroke-linecap'];
-    const dasharray = rule['stroke-dasharray'];
-    const dashOffset = rule['stroke-dashoffset'];
-    // graphicStroke
-    // perpendicularOffset
-    // graphicFill
+  getScaleDenominatorFromSelectors(scales: any): ScaleDenominator | undefined {
+    return undefined;
+  }
 
+  readExpression(expression: any) {
+    if (expression === undefined) {
+      return null;
+    }
+    if (typeof expression === 'string' || expression instanceof String) {
+        return expression;
+    }
+    if (!isNaN(expression)) {
+        return expression;
+    }
+    const [ operator, ...args ] = expression;
+
+    switch (operator) {
+      case 'array':
+        return args;
+      case 'get':
+        return args[0];
+      case 'hex':
+        return args[0];
+      case 'symbol':
+        return args[0];
+      case 'url':
+        return args[0];
+      default:
+        return expression;
+    }
+  }
+
+  getMarkSymbolizerFromGeoCSSRule(properties: any): MarkSymbolizer {
+    const symbolName = this.readExpression(properties.mark);
+    let wellKnownName:WellKnownName;
+    switch (symbolName) {
+      case 'circle':
+      case 'square':
+      case 'triangle':
+      case 'star':
+      case 'cross':
+      case 'x':
+        const wkn = symbolName.charAt(0).toUpperCase() + symbolName.slice(1);
+        wellKnownName = wkn as WellKnownName;
+        break;
+      case 'shape://vertline':
+      case 'shape://horline':
+      case 'shape://slash':
+      case 'shape://backslash':
+      case 'shape://dot':
+      case 'shape://plus':
+      case 'shape://times':
+      case 'shape://oarrow':
+      case 'shape://carrow':
+        wellKnownName = symbolName as WellKnownName;
+        break;
+      default:
+        throw new Error('MarkSymbolizer cannot be parsed. Unsupported WellKnownName.');
+    }
+
+    const kind = 'Mark';
+
+    const markSize = this.readExpression(properties['mark-size']);
+    const markRotation = this.readExpression(properties['mark-rotation']);
+
+    const pseudoSelector = properties[':mark'] || {};
+
+    const opacity = this.readExpression(pseudoSelector.opacity);
+    const fillOpacity = this.readExpression(pseudoSelector['fill-opacity']);
+    const color = this.readExpression(pseudoSelector.fill);
+    const rotate = this.readExpression(pseudoSelector.rotation) || markRotation;
+    const size = (this.readExpression(pseudoSelector.size) || markSize);
+    const strokeColor = this.readExpression(pseudoSelector.stroke);
+    const strokeWidth = this.readExpression(pseudoSelector['stroke-width']);
+    const strokeOpacity = this.readExpression(pseudoSelector['stroke-opacity']);
+
+    const wellKnownNameParam = wellKnownName && { wellKnownName };
+    const opacityParam = !_isNil(opacity) && { opacity };
+    const fillOpacityParam = !_isNil(fillOpacity) && { fillOpacity };
     const colorParam = color && { color };
-    const widthParam = width && { width };
-    const opacityParam = opacity && { opacity };
+    const rotateParam = !_isNil(rotate) && { rotate };
+    const radiusParam = !_isNil(size) && { radius: size / 2 };
+    const strokeColorParam = strokeColor && { strokeColor };
+    const strokeWidthParam = !_isNil(strokeWidth) && { strokeWidth };
+    const strokeOpacityParam = !_isNil(strokeOpacity) && { strokeOpacity };
+
+    return {
+      kind,
+      ...wellKnownNameParam,
+      ...opacityParam,
+      ...fillOpacityParam,
+      ...colorParam,
+      ...rotateParam,
+      ...radiusParam,
+      ...strokeColorParam,
+      ...strokeWidthParam,
+      ...strokeOpacityParam
+    };
+  }
+
+  getIconSymbolizerFromGeoCSSRule(properties: any): IconSymbolizer {
+    const kind = 'Icon';
+    const image = this.readExpression(properties.mark);
+    const markSize = this.readExpression(properties['mark-size']);
+    const markRotation = this.readExpression(properties['mark-rotation']);
+
+    const pseudoSelector = properties[':mark'] || {};
+
+    const opacity = this.readExpression(pseudoSelector.opacity);
+    const rotate = this.readExpression(pseudoSelector.rotation) || markRotation;
+    const size = this.readExpression(pseudoSelector.size) || markSize;
+
+    const imageParam = image && { image };
+    const opacityParam = !_isNil(opacity) && { opacity };
+    const rotateParam = !_isNil(rotate) && { rotate };
+    const sizeParam = !_isNil(size) && { size };
+
+    return {
+      kind,
+      ...imageParam,
+      ...opacityParam,
+      ...rotateParam,
+      ...sizeParam
+    };
+  }
+
+  getPointSymbolizerFromGeoCSSRule(properties: any): PointSymbolizer {
+    const externalGraphic = properties.mark[0] === 'url';
+    if (externalGraphic) {
+      return this.getIconSymbolizerFromGeoCSSRule(properties);
+    }
+    return this.getMarkSymbolizerFromGeoCSSRule(properties);
+  }
+
+  getLineSymbolizerFromGeoCSSRule(properties: any): LineSymbolizer {
+    const kind = 'Line';
+    const color = this.readExpression(properties.stroke);
+    const width = this.readExpression(properties['stroke-width']);
+    const opacity = this.readExpression(properties['stroke-opacity']);
+    const join = this.readExpression(properties['stroke-linejoin']);
+    const cap = this.readExpression(properties['stroke-linecap']);
+    const dasharray = this.readExpression(properties['stroke-dasharray']);
+    const dashOffset = this.readExpression(properties['stroke-dashoffset']);
+    const perpendicularOffset = this.readExpression(properties['stroke-offset']);
+
+    // graphicFill or graphicStroke
+    const graphicStroke = (properties.stroke[0] === 'symbol' || properties.stroke[0] === 'url')
+      ? this.getPointSymbolizerFromGeoCSSRule({
+        'mark': properties.stroke,
+        'mark-size': properties['stroke-size'],
+        'mark-rotation': properties['stroke-rotation'],
+        ':mark': properties[':stroke']
+      })
+      : undefined;
+
+    const colorParam = !graphicStroke && color && { color };
+    const graphicStrokeParam = graphicStroke && { graphicStroke };
+    const widthParam = !_isNil(width) && { width };
+    const opacityParam = !_isNil(opacity) && { opacity };
     const joinParam = join && { join };
     const capParam = cap && { cap };
     const dasharrayParam = dasharray && { dasharray };
     const dashOffsetParam = dashOffset && { dashOffset };
+    const perpendicularOffsetParam = !_isNil(perpendicularOffset) && { perpendicularOffset };
 
     return {
       kind,
       ...colorParam,
+      ...graphicStrokeParam,
       ...widthParam,
       ...opacityParam,
       ...joinParam,
       ...capParam,
       ...dasharrayParam,
-      ...dashOffsetParam
+      ...dashOffsetParam,
+      ...perpendicularOffsetParam
     };
   }
 
-  getFillSymbolizerFromGeoCSSRule(geoCSSRule: any): FillSymbolizer {
-    const { rule } = geoCSSRule;
-    const kind = 'Fill';
-    const color = rule.fill;
-    const fillOpacity = rule['fill-opacity'] && parseFloat(rule['fill-opacity']);
-    const outlineColor = rule.stroke;
-    const outlineWidth = rule['stroke-width'] && parseFloat(rule['stroke-width']);
-    const outlineOpacity = rule['stroke-opacity'] && parseFloat(rule['stroke-opacity']);
-    const outlineDasharray = rule['stroke-dasharray'] && rule['stroke-dasharray']
-      .split(' ')
-      .map((value: string) => parseFloat(value));
-    // graphicFill
+  getFillSymbolizerFromGeoCSSRule(properties: any): FillSymbolizer {
 
-    const colorParam = color && { color };
+    const kind = 'Fill';
+    const color = this.readExpression(properties.fill);
+    const fillOpacity = this.readExpression(properties['fill-opacity']);
+    const outlineColor = this.readExpression(properties.stroke);
+    const outlineWidth = this.readExpression(properties['stroke-width']);
+    const outlineOpacity = this.readExpression(properties['stroke-opacity']);
+    const outlineDasharray = this.readExpression(properties['stroke-dasharray']);
+
+    const graphicFill = (properties.fill[0] === 'symbol' || properties.fill[0] === 'url')
+      ? this.getPointSymbolizerFromGeoCSSRule({
+        'mark': properties.fill,
+        'mark-size': properties['fill-size'],
+        'mark-rotation': properties['fill-rotation'],
+        ':mark': properties[':fill']
+      })
+      : undefined;
+
+    const colorParam = !graphicFill && color && { color };
+    const graphicFillParam = graphicFill && { graphicFill };
     const opacityParam = !color && { opacity: 0 };
-    const fillOpacityParam = fillOpacity && { fillOpacity };
+    const fillOpacityParam = !_isNil(fillOpacity) && { fillOpacity };
     const outlineColorParam = outlineColor && { outlineColor };
-    const outlineWidthParam = outlineWidth && { outlineWidth };
-    const outlineOpacityParam = outlineOpacity && { outlineOpacity };
+    const outlineWidthParam = !_isNil(outlineWidth) && { outlineWidth };
+    const outlineOpacityParam = !_isNil(outlineOpacity) && { outlineOpacity };
     const outlineDasharrayParam = outlineDasharray && { outlineDasharray };
 
     return {
       kind,
       ...colorParam,
+      ...graphicFillParam,
       ...opacityParam,
       ...fillOpacityParam,
       ...outlineColorParam,
@@ -131,28 +266,135 @@ export class GeoCSSStyleParser implements StyleParser {
     };
   }
 
-  getSymbolizersFromRules(geoCSSRules: any): Symbolizer[] {
-    const { rules, pseudoSelectors } = geoCSSRules;
-    return rules.reduce(
-      (acc: Symbolizer[], rule: any) => {
-        if (rule.symbolizer === 'line') {
-          return [...acc, this.getLineSymbolizerFromGeoCSSRule({ rule, pseudoSelectors })];
-        }
-        if (rule.symbolizer === 'polygon') {
-          return [...acc, this.getFillSymbolizerFromGeoCSSRule({ rule, pseudoSelectors })];
-        }
-        return [...acc];
-      },
-      []);
+  getTextSymbolizerFromGeoCSSRule(properties: any): TextSymbolizer {
+    const kind = 'Text';
+
+    const label = this.readExpression(properties.label); // this.getTextSymbolizerLabelFromGeoCSSProperty(label);
+    const color = this.readExpression(properties['font-fill']);
+    const haloWidth = this.readExpression(properties['halo-radius']);
+    const haloColor = this.readExpression(properties['halo-color']);
+    const offset = this.readExpression(properties['label-offset']);
+    const rotate = this.readExpression(properties['label-rotation']);
+    const font = this.readExpression(properties['font-family']);
+    const fontStyle = this.readExpression(properties['font-style']);
+    const fontWeight = this.readExpression(properties['font-weight']);
+    const size = this.readExpression(properties['font-size']);
+
+    const labelParam = label && { label };
+    const colorParam = color && { color };
+    const haloWidthParam = !_isNil(haloWidth) && { haloWidth };
+    const haloColorParam = haloColor && { haloColor };
+    const offsetParam = offset && { offset };
+    const rotateParam = !_isNil(rotate) && { rotate };
+    const fontParam = font && { font: _castArray(font) };
+    const fontStyleParam = fontStyle && { fontStyle };
+    const fontWeightParam = fontWeight && { fontWeight };
+    const sizeParam = !_isNil(size) && { size };
+
+    return {
+      kind,
+      ...labelParam,
+      ...colorParam,
+      ...haloWidthParam,
+      ...haloColorParam,
+      ...offsetParam,
+      ...rotateParam,
+      ...fontParam,
+      ...fontStyleParam,
+      ...fontWeightParam,
+      ...sizeParam
+    };
   }
 
-  getRulesFromGeoCSSSelectors(geoCSSSelectors: any): Rule[] {
-    const { pseudoSelectors, selectors } = geoCSSSelectors;
-    return selectors.map((geoCSSSelector: any) => {
-      const { title = '', selectors: filterSelectors, rules } = geoCSSSelector;
+  getRasterSymbolizerFromGeoCSSRule(properties: any): RasterSymbolizer {
+    const kind = 'Raster';
+
+    const opacity = this.readExpression(properties['']);
+    const colorMap = this.readExpression(properties['']); // this.getColorMapFromGeoCSSProperty();
+    const channelSelection = this.readExpression(properties['']); // this.getChannelSelectionFromGeoCSSPropertyChannelSelection();
+    const contrastEnhancement = this.readExpression(properties['']); // this.getContrastEnhancementFromGeoCSSPropertyContrastEnhancement();
+
+    const opacityParam = !_isNil(opacity) && { opacity };
+    const colorMapParam = colorMap && { colorMap };
+    const channelSelectionParam = channelSelection && { channelSelection };
+    const contrastEnhancementParam = contrastEnhancement && { contrastEnhancement };
+
+    return {
+      kind,
+      ...opacityParam,
+      ...colorMapParam,
+      ...channelSelectionParam,
+      ...contrastEnhancementParam
+    };
+  }
+
+  getSymbolizerTypesFromProperties(properties: any): String[] {
+    let symbolizers = [];
+    if (properties.mark) {
+      symbolizers.push('point');
+    } else if (properties.stroke && (properties.fill === 'none' || !properties.fill)) {
+      symbolizers.push('line');
+    } else if (properties.fill && properties.fill !== 'none') {
+      symbolizers.push('polygon');
+    }
+    if (properties.label) { symbolizers.push('text'); }
+    return symbolizers;
+  }
+
+  getSymbolizersFromRules(geoCSSProperties: any): Symbolizer[] {
+    const propertiesGroup = _castArray(geoCSSProperties);
+    return propertiesGroup.reduce((symbolizers: Symbolizer[], properties: any) => {
+      const symbolizerTypes = this.getSymbolizerTypesFromProperties(properties);
+      return [
+        ...symbolizers,
+        ...symbolizerTypes.reduce(
+          (acc: Symbolizer[], symbolizerType: string) => {
+            if (symbolizerType === 'point') {
+              return [...acc, this.getPointSymbolizerFromGeoCSSRule(properties)];
+            }
+            if (symbolizerType === 'line') {
+              return [...acc, this.getLineSymbolizerFromGeoCSSRule(properties)];
+            }
+            if (symbolizerType === 'polygon') {
+              return [...acc, this.getFillSymbolizerFromGeoCSSRule(properties)];
+            }
+            if (symbolizerType === 'text') {
+              return [...acc, this.getTextSymbolizerFromGeoCSSRule(properties)];
+            }
+            return [...acc];
+          },
+          [])
+      ]
+    }, []);
+  }
+
+  getRulesFromGeoCSSSelectors(geoCSSRules: any): Rule[] {
+
+    // merge rule with same group id
+    // to render as symbolizer
+    let rules = [];
+    for (let i = 0; i < geoCSSRules.length; i++) {
+      const rule = geoCSSRules[i];
+      const lastGroup:any = rules[rules.length - 1] || {};
+      const lastGroupId = lastGroup.group;
+      if (lastGroupId !== undefined && lastGroupId === rule.group) {
+        rules[rules.length - 1] = {
+          ...lastGroup,
+          properties: [
+            ...(_isArray(lastGroup.properties) ? lastGroup.properties : [lastGroup.properties]),
+            rule.properties
+          ]
+        };
+      } else {
+        rules.push(rule);
+      }
+    }
+
+    return rules.map((geoCSSRule: any) => {
+      const { title = '', selector: filterSelectors, properties, ...others } = geoCSSRule;
       const filter: Filter | undefined = this.getFilterFromSelectors(filterSelectors);
-      const scaleDenominator: ScaleDenominator | undefined = this.getScaleDenominatorFromSelectors();
-      const symbolizers: Symbolizer[] = this.getSymbolizersFromRules({ rules, pseudoSelectors });
+      const scaleDenominator: ScaleDenominator | undefined = this.getScaleDenominatorFromSelectors(others);
+      const symbolizers: Symbolizer[] = this.getSymbolizersFromRules(properties);
       const filterParam = filter && { filter };
       const scaleDenominatorParam = scaleDenominator && { scaleDenominator };
       const symbolizersParam = symbolizers && { symbolizers };
@@ -166,12 +408,12 @@ export class GeoCSSStyleParser implements StyleParser {
   }
 
   geoCSSObjectToGeoStylerStyle(geoCSSObject: any): Style {
-    const { info = {}, ...geoCSSSelectors } = geoCSSObject;
-    if (info.mode !== 'Flat') {
+    const { directive = {}, rules: geoCSSRules } = geoCSSObject;
+    if (directive['@mode'] !== 'Flat') {
       this.warn('Supported only \'Flat\' mode, other directive will be translated as flat GeoCSS');
     }
-    const name = info.styleTitle || '';
-    const rules = this.getRulesFromGeoCSSSelectors(geoCSSSelectors);
+    const name = directive['@styleTitle'] || '';
+    const rules = this.getRulesFromGeoCSSSelectors(geoCSSRules);
     return {
       name,
       rules
@@ -181,7 +423,7 @@ export class GeoCSSStyleParser implements StyleParser {
   readStyle(geoCSSStyle: string): Promise<Style> {
     return new Promise((resolve, reject) => {
       try {
-        const geoCSSObject = geoCSS2JSON(geoCSSStyle);
+        const geoCSSObject = flatGeoCSS.read(geoCSSStyle);
         const geoStylerStyle = this.geoCSSObjectToGeoStylerStyle(geoCSSObject);
         resolve(geoStylerStyle);
       } catch (error) {
